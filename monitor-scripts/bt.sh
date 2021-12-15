e7b4600803cecde8353cbd8be29f22617fbe5a70
briffy <40440683+briffy@users.noreply.github.com>
2021-12-15 13:37:47 +0000

    Delete monitor-scripts directory

--- monitor-scripts/bt.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-data=$(sudo /opt/gateway_config/bin/gateway_config advertise status)
-
-echo $data > /var/dashboard/statuses/bt
--- monitor-scripts/update-check.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-wget --no-cache https://raw.githubusercontent.com/briffy/PantherDashboard/main/version -O /var/dashboard/update
