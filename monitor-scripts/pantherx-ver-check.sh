9fa6cba89225ee88011d857f314e3966ec962611
qinfengling <Fengling.Qin@gmail.com>
2022-01-28 14:45:52 +0800

    Fix panther version detect

--- monitor-scripts/pantherx-ver-check.sh
+++ monitor-scripts/pantherx-ver-check.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if test -f /opt/miner_data; then
-  echo "X1" > /var/dashboard/statuses/pantherx_ver
+if test -d /opt/miner_data; then
+  echo -n "X1" > /var/dashboard/statuses/pantherx_ver
 fi
 
-if test -f /opt/panther-x2/miner_data; then
-  echo "X2" > /var/dashboard/statuses/pantherx_ver
+if test -d /opt/panther-x2/miner_data; then
+  echo -n "X2" > /var/dashboard/statuses/pantherx_ver
 fi
