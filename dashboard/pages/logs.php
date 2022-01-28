ebb95e5f90dd98861ec8143c2f8bf21fcdb40eac
qinfengling <Fengling.Qin@gmail.com>
2022-01-28 17:54:09 +0800

    Fix invalid log read

--- dashboard/pages/logs.php
+++ dashboard/pages/logs.php
@@ -1,8 +1,17 @@
 <?php
-$logs['miner'] = shell_exec('docker exec helium-miner tail -300 /var/data/log/console.log | tac');
-$logs['witnesses'] = shell_exec('docker exec helium-miner  tac /var/data/log/console.log | grep -E "miner_onion_server:send_witness|miner_onion_server:decrypt|client sending data"');
-$logs['errors'] = shell_exec('docker exec helium-miner tail -100 /var/data/log/error.log | tac');
 $info['PantherXVer'] = trim(file_get_contents("/var/dashboard/statuses/pantherx_ver"));
+
+if ($info['PantherXVer'] == 'X1') {
+	$logs['miner'] = shell_exec('tail -300 /opt/miner_data/log/console.log | tac');
+	$logs['witnesses'] = shell_exec('tac /opt/miner_data/log/console.log | grep -E "miner_onion_server:send_witness|miner_onion_server:decrypt|client sending data"');
+	$logs['errors'] = shell_exec('tail -100 /opt/miner_data/log/error.log | tac');
+}
+
+if ($info['PantherXVer'] == 'X2') {
+	$logs['miner'] = shell_exec('tail -300 /opt/panther-x2/miner_data/log/console.log | tac');
+	$logs['witnesses'] = shell_exec('tac /opt/panther-x2/miner_data/log/console.log | grep -E "miner_onion_server:send_witness|miner_onion_server:decrypt|client sending data"');
+	$logs['errors'] = shell_exec('tail -100 /opt/panther-x2/miner_data/log/error.log | tac');
+}
 ?>
 <h1>Panther <?php echo $info['PantherXVer']; ?> Miner Dashboard - Logs</h1>
 
