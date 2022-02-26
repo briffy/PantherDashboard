2c2e6f8d3117b9ab0ee4948a09457d3bf88f433a
qinfengling <Fengling.Qin@gmail.com>
2022-02-27 00:19:22 +0800

    Fix Uncaught Error: Call to undefined function json_encode()

--- dashboard/public/HeliumStatus.php
+++ dashboard/public/HeliumStatus.php
@@ -1,11 +1,10 @@
 <?php
-	shell_exec("/var/dashboard/monitor-scripts/helium-statuses.sh");
+	shell_exec("/etc/monitor-scripts/helium-statuses.sh");
 	$live_height = trim(file_get_contents('/var/dashboard/statuses/current_blockheight'));
 	$online_status = trim(file_get_contents("/var/dashboard/statuses/online_status"));
 	$miner_height = trim(file_get_contents("/var/dashboard/statuses/infoheight"));
 
 	header('Content-Type:application/json; charset=utf-8');
-	$arr = array('live_height'=>$live_height,'online_status'=>$online_status,'miner_height'=>$miner_height);
 
-	echo json_encode($arr);
+	echo '{"live_height":"'.$live_height.'","online_status":"'.$online_status.'","miner_height":"'.$miner_height.'"}';
 ?>
