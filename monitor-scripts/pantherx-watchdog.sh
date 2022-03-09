a5cb2d9b1067ab20149ec24d1f68c8b36cbdbe80
qinfengling <Fengling.Qin@gmail.com>
2022-03-09 20:35:20 +0800

    Update cconnmanctl fix check

--- monitor-scripts/pantherx-watchdog.sh
+++ monitor-scripts/pantherx-watchdog.sh
@@ -44,9 +44,9 @@ if [[ "$pantherx_ver" = "X2" ]]; then
 ## Fix typo in /usr/local/sbin/init_wifi.sh
 touch /tmp/pantherx_watchdog.log
 echo "fix typo in /usr/local/sbin/init_wifi.sh for Panther X2" > /tmp/pantherx_watchdog.log
-echo "1b887ffc3a792d93399d5cccd6242d67 /usr/local/sbin/init_wifi.sh" | md5sum -c
+grep "cconnmanctl" /usr/local/sbin/init_wifi.sh
 retval=$?
-if [ $retval -ne 0 ]; then
+if [ $retval -eq 0 ]; then
     sed -i 's/\/usr\/bin\/cconnmanctl/\/usr\/bin\/connmanctl/g' /usr/local/sbin/init_wifi.sh
     systemctl restart wifi_init.service
 fi
