6ef2a48f097ac36fd2d74ec0c6b71361cf516635
qinfengling <Fengling.Qin@gmail.com>
2022-03-11 14:08:38 +0800

    Fix update dashboard failed after a sudden power failure

--- monitor-scripts/dashboard-update.sh
+++ monitor-scripts/dashboard-update.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+name="dashboard-update"
 service=$(cat /var/dashboard/services/dashboard-update | tr -d '\n')
 
+# Fix invalid status when boot finish
+if [[ $service == 'running' ]]; then
+  if [[ ! -f /tmp/dashboard-$name-flag ]]; then
+    echo 'stopped' > /var/dashboard/services/$name
+  fi
+fi
+
 if [[ $service == 'start' ]]; then
-  echo 'running' > /var/dashboard/services/dashboard-update
+  touch /tmp/dashboard-$name-flag
+  echo 'running' > /var/dashboard/services/$name
   wget https://raw.githubusercontent.com/Panther-X/PantherDashboard/main/update.sh -O - | sudo bash
 fi
