f5c91a845cce9362bad5954972b42a7ecc1598c1
qinfengling <Fengling.Qin@gmail.com>
2022-03-16 16:09:03 +0800

    Fix typo in temperature measure

--- monitor-scripts/temp.sh
+++ monitor-scripts/temp.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-raw=`awk -v var1=$(</sys/class/thermal/thermal_zone0/temp) -v var2=1000 'BEGIN { print ( var1 / var2 ) }'
-{ print ( var1 / var2 ) }'`
+raw=`awk -v var1=$(</sys/class/thermal/thermal_zone0/temp) -v var2=1000 'BEGIN { print ( var1 / var2 ) }'`
 data=`printf %0.2f $raw`
 
 echo ${data}\'C > /var/dashboard/statuses/temp
