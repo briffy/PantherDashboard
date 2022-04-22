2af6688b916b71166d58bf7d03788558e1a44d58
qinfengling <Fengling.Qin@gmail.com>
2022-04-22 10:54:54 +0800

    Fix miner verison check with quay.io server

--- monitor-scripts/miner-version-check.sh
+++ monitor-scripts/miner-version-check.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-if test -f /var/dashboard/branch; then
-  BRANCH=`cat /var/dashboard/branch`
-else
-  BRANCH='main'
-fi
+latest=$(curl -s 'https://quay.io/api/v1/repository/team-helium/miner/tag/?limit=10&page=1&onlyActiveTags=true' \
+	  -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36' \
+	    -H 'x-requested-with: XMLHttpRequest' | grep -Po 'miner-arm64_[0-9]+\.[0-9]+\.[0-9]+\.[^"]+_GA' | sort -n | tail -1)
 
-wget --no-cache https://raw.githubusercontent.com/Panther-X/PantherDashboard/${BRANCH}/latest_miner_version -O /var/dashboard/statuses/latest_miner_version
+echo -n $latest > /var/dashboard/statuses/latest_miner_version
 docker ps --format "{{.Image}}" --filter "name=helium-miner" | grep -Po "miner-arm64_.*" > /var/dashboard/statuses/current_miner_version
