3ee44671b41b1140b674a1fff65ae7512b5d2f25
qinfengling <Fengling.Qin@gmail.com>
2022-06-04 13:13:03 +0800

    Fix corrupt php7.3-json
    Note: It was found in lorapacketforwarderanalyzer.php
    PHP Fatal error:  Uncaught Error: Call to undefined function json_decode()

--- update.sh
+++ update.sh
@@ -27,6 +27,11 @@ if id -nG admin | grep -qw "sudo"; then
     cd PantherDashboard-${VER}
 
     apt-get update
+    # check php7.3-json, need to do it before php-fpm installation
+    if ! test -s /etc/php/7.3/mods-available/json.ini; then
+      # Remove it if is corrupt, it will install with php-fpm
+      apt-get --assume-yes purge php7.3-json
+    fi
     apt-get --assume-yes install nginx php-fpm php7.3-fpm ngrep gawk php-cli
 
     mkdir -p /var/dashboard
