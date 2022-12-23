0dcc5c5aaf3475359c2c8eacce7b5dc5595bfec8
qinfengling <Fengling.Qin@gmail.com>
2022-12-23 12:05:37 +0800

    Check hotspot online status when pubkey is valid

--- monitor-scripts/helium-statuses.sh
+++ monitor-scripts/helium-statuses.sh
@@ -6,13 +6,12 @@ uri="$root_uri$pubkey"
 grep "online" /var/dashboard/statuses/online_status
 retval=$?
 if [ $retval -ne 0 ]; then
-  if data=$(wget -U "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36" $uri -qO-); then
-    online_status=$(echo $data | grep -Po '"online":".*?[^\\]"' | sed -e 's/^"online"://' | tr -d '"')
-  else
-    online_status="Not Onboarded"
-  fi
-
   if [[ $pubkey ]]; then
+    if data=$(wget -U "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36" $uri -qO-); then
+      online_status=$(echo $data | grep -Po '"online":".*?[^\\]"' | sed -e 's/^"online"://' | tr -d '"')
+    else
+      online_status="Not Onboarded"
+    fi
     echo $online_status > /var/dashboard/statuses/online_status
   else
     echo 'unknown' > /var/dashboard/statuses/online_status
