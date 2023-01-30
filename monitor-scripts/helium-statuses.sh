75e4185c4ce37f61bafabdbf1b59859794e4e213
xuzhenxing <xrnmail@163.com>
2023-01-30 18:24:02 +0800

    Fix hotspot online status check method

--- monitor-scripts/helium-statuses.sh
+++ monitor-scripts/helium-statuses.sh
@@ -3,21 +3,14 @@ pubkey=$(cat /var/dashboard/statuses/pubkey | tr -d "\n")
 root_uri='https://api.helium.io/v1/hotspots/'
 uri="$root_uri$pubkey"
 
-grep "online" /var/dashboard/statuses/online_status
-retval=$?
-if [ $retval -ne 0 ]; then
-  if [[ $pubkey ]]; then
-    if data=$(wget -U "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36" $uri -qO-); then
-      online_status=$(echo $data | grep -Po '"online":".*?[^\\]"' | sed -e 's/^"online"://' | tr -d '"')
-    else
-      online_status="Not Onboarded"
-    fi
+if [[ $pubkey ]]; then
+  if data=$(wget -U "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36" $uri -qO-); then
+    online_status=$(echo $data | grep -Po '"online":".*?[^\\]"' | sed -e 's/^"online"://' | tr -d '"')
     echo $online_status > /var/dashboard/statuses/online_status
-  else
-    echo 'unknown' > /var/dashboard/statuses/online_status
   fi
+else
+  echo 'unknown' > /var/dashboard/statuses/online_status
 fi
 
 height=$(wget -U "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36" 'https://api.helium.io/v1/blocks/height' -qO- | grep -Po '"height":[^}]+' | sed -e 's/^"height"://')
-
 echo $height > /var/dashboard/statuses/current_blockheight
